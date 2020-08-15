67bba90cd8600df2d8567ea87cb186509036fa18
Ieva Stripkāne <ieva.stripkane.2002@gmail.com>
2020-08-15 18:51:06 +0300

    version 18:50

--- Enemy.cs
+++ Enemy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,43 @@ namespace PacmanElements
 {
     class Enemy : PictureBox
     {
+        public int Step { get; set; } = 2;
+        public int HorizontalVelocity { get; set; } = 0;
+        public int VerticalVelocity { get; set; } = 2;
+
+        public Enemy()
+        {
+            InitializeEnemy();
+        }
+        private void InitializeEnemy()
+        {
+            this.BackColor = Color.Red;
+            this.Size = new Size(40, 40);
+            this.Tag = "enemy";
+        }
+
+        public void SetRandomDirection()
+        {
+            int directionCode = rand.Next(1, 5);
+            switch (directionCode)
+            {
+                case 1:
+                    HorizontalVelocity = Step;
+                    VerticalVelocity = 0;
+                case 2:
+                    HorizontalVelocity = 0;
+                    VerticalVelocity = Step;
+                case 3:
+                    HorizontalVelocity = Step;
+                    VerticalVelocity = 0;
+                case 4:
+                    HorizontalVelocity = Step;
+                    VerticalVelocity = 0;
+
+
+            }
+        }
+
     }
+
 }
--- Game.cs
+++ Game.cs
@@ -12,9 +12,13 @@ namespace PacmanElements
 {
     public partial class Game : Form
     {
+        private int initializeEnemyCount = 4;
+
+        private Random rand = new Random();
         private Level level = new Level();
         private Hero hero = new Hero();
         private Timer mainTimer = null;
+        private List<Enemy> enemies = new List<Enemy>();
         public Game()
         {
             InitializeComponent();
@@ -28,11 +32,22 @@ namespace PacmanElements
             this.Size = new Size(500, 500);
             //add key down event handler
             this.KeyDown += Game_KeyDown;
-            //adding level to the game
-            this.Controls.Add(level);
+            AddLevel();
+            AddHero();
+            AddEnemies();
+        }
+
+        private void AddHero()
+        {
             //adding hero to the game
             this.Controls.Add(hero);
             hero.BringToFront();
+        }
+
+        private void AddLevel()
+        {
+            //adding level to the game
+            this.Controls.Add(level);
         }
 
         private void InitializeMainTimer()
@@ -45,9 +60,25 @@ namespace PacmanElements
 
         private void MainTimer_Tick(object sender, EventArgs e)
         {
+            MoveHero();
+            HeroBorderCollision();
+            MoveEnemies();
+        }
+
+        private void MoveHero()
+        {
             hero.Left += hero.HorizontalVelocity;
             hero.Top += hero.VerticalVelocity;
             HeroBorderCollision();
+        }
+
+        private void MoveEnemies()
+        {
+            foreach(var enemy in enemies)
+            {
+                enemy.Left += enemy.HorizontalVelocity;
+                enemy.Top += enemy.VerticalVelocity;
+            }
         }
 
         private void Game_KeyDown(object sender, KeyEventArgs e)
@@ -90,6 +121,19 @@ namespace PacmanElements
             if (hero.Top + hero.Height < level.Top)
             {
                 hero.Top = level.Top + level.Height;
+            }
+        }
+
+        private void AddEnemies()
+        {
+            Enemy enemy;
+            for(int i = 0; i < initializeEnemyCount; i++)
+            {
+                enemy = new Enemy();
+                enemy.Location = new Point(rand.Next(100, 500), rand.Next(100, 500));
+                enemies.Add(new Enemy());
+                this.Controls.Add(enemy);
+                enemy.BringToFront();
             }
         }
     }
